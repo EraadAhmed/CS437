9935b36ab64630e047e8b078efaa3fe315eefef5
eahme2 <eahme2@illinois.edu>
2025-09-21 11:57:27 -0500

    computer vision changes made

--- Lab1_PartB/computer_vision.py
+++ Lab1_PartB/computer_vision.py
@@ -22,29 +22,43 @@ SAMPLING = 1 #everything scaled down by factor of 5
 
 def calibrate(picarx, current_pos, map_grid):
     #calibrate positioning of ultrasonic sensor relative velocity
+    picarx.set_cam_pan_angle(angle)
+    time.sleep(3)
     angle = -60
     while angle <= 60:
         time.sleep(0.1)
-        picarx.set_cam_pan_angle(angle) 
-        reading = int(picarx.ultrasonic.read()) #gets distance reading in cm
+        picarx.set_cam_pan_angle(angle)
+        reading = picarx.ultrasonic.read() #gets distance reading in cm
+        if 0 < reading <= MAXREAD:
+            time.sleep((reading*1e-2)/343)
+        else: continue
         
-        time.sleep(0.01)
-        if angle == 0 and reading <= MAXREAD:
-            reading_0 =  int(np.ceil(reading / 5.0) * 5) #scales it toto nearest divisible by 5
-            map[current_pos[0]][reading_0+current_pos[1]] = 1
-        elif angle < 0:
-            if(reading <= current_pos[0]/np.cos(angle) and reading <= MAXREAD):
-                object_xval = int(np.ceil((current_pos[0] - reading * np.cos(np.radians(angle)))/5)*5)
-                object_yval = int(np.ceil((current_pos[1] + reading * np.sin(np.radians(abs(angle))))/5)*5)
-                map[object_xval][object_yval] = 1
-        elif angle > 0:
-            if(reading <= (WIDTH - current_pos[0])/np.cos(angle) and reading <= MAXREAD):
-                object_xval = int(np.ceil((current_pos[0] + reading * np.cos(np.radians(angle)))/5)*5)
-                object_yval = int(np.ceil((current_pos[1] + reading * np.sin(np.radians(abs(angle))))/5)*5)
-                map[object_xval][object_yval] = 1
-        
+        zero = np.nonzero(map_grid)
+        print(angle, reading, list(zip(*zero)))
+        object_xval, object_yval = None, None
+
+         # --- Straight ahead ---
+        if angle == 0:
+            object_xval = current_pos[0]
+            reading_0 =  int(np.ceil(reading / SAMPLING)) #scales it toto nearest divisible by 5
+            object_yval = (reading_0) + current_pos[1]
+            if object_yval < LENGTH:
+                print("tuple: ", (object_xval, object_yval), end="\n\n\n\n")
+                map_grid[object_xval][object_yval] = 1
+                print(object_xval, object_yval)
+
+        # --- Left side (< 0)  or right side (>0) ---
+        else:
+            object_xval = int(np.ceil((current_pos[0] + int(np.sign(angle)) * reading * np.sin(np.radians(90-abs(angle))))/SAMPLING)) # np.sign will decide to add to subtract from the x coord
+            object_yval = int(np.ceil((current_pos[1] + reading * np.cos(np.radians(90 - abs(angle))))/SAMPLING))
+            if (0 <= object_xval < WIDTH and 0 <= object_yval < LENGTH):
+                print("tuple: ", (object_xval, object_yval), end="\n\n\n\n")
+                map_grid[object_xval][object_yval] = 1
+                print(object_xval, object_yval)
+            
         angle += 5
     picarx.set_cam_pan_angle(0)
+    time.sleep(2)
     return map
 
 
@@ -62,31 +76,34 @@ async def ultrasonic_pan_loop(picarx, current_pos, map_grid):
         time.sleep(0.1)
         picarx.set_cam_pan_angle(angle)
         reading = picarx.ultrasonic.read() #gets distance reading in cm
+
+        if 0 < reading <= MAXREAD:
+            time.sleep((reading*1e-2)/343)
+        else: continue
+
         zero = np.nonzero(map_grid)
         print(angle, reading, list(zip(*zero)))
-        time.sleep(current_pos[0]/np.cos(np.radians(90-angle))/343)
-
         object_xval, object_yval = None, None
 
-        # --- Straight ahead ---
-        if 0 < reading <= MAXREAD:
-            if angle == 0:
-                object_xval = current_pos[0]
-                reading_0 =  int(np.ceil(reading / SAMPLING)) #scales it toto nearest divisible by 5
-                object_yval = (reading_0) + current_pos[1]
-                if object_yval < LENGTH:
-                    print("tuple: ", (object_xval, object_yval), end="\n\n\n\n")
-                    map_grid[object_xval][object_yval] = 1
+         # --- Straight ahead ---
+        if angle == 0:
+            object_xval = current_pos[0]
+            reading_0 =  int(np.ceil(reading / SAMPLING)) #scales it toto nearest divisible by 5
+            object_yval = (reading_0) + current_pos[1]
+            if object_yval < LENGTH:
+                print("tuple: ", (object_xval, object_yval), end="\n\n\n\n")
+                map_grid[object_xval][object_yval] = 1
+                print(object_xval, object_yval)
 
-            # --- Left side (< 0)  or right side (>0) ---
-            else:
-                object_xval = int(np.ceil((current_pos[0] + int(np.sign(angle)) * reading * np.sin(np.radians(90-abs(angle))))/SAMPLING)) # np.sign will decide to add to subtract from the x coord
-                object_yval = int(np.ceil((current_pos[1] + reading * np.cos(np.radians(90 - abs(angle))))/SAMPLING))
-                if (0 <= object_xval < WIDTH and 0 <= object_yval < LENGTH):
-                    print("tuple: ", (object_xval, object_yval), end="\n\n\n\n")
-                    map_grid[object_xval][object_yval] = 1
+        # --- Left side (< 0)  or right side (>0) ---
+        else:
+            object_xval = int(np.ceil((current_pos[0] + int(np.sign(angle)) * reading * np.sin(np.radians(90-abs(angle))))/SAMPLING)) # np.sign will decide to add to subtract from the x coord
+            object_yval = int(np.ceil((current_pos[1] + reading * np.cos(np.radians(90 - abs(angle))))/SAMPLING))
+            if (0 <= object_xval < WIDTH and 0 <= object_yval < LENGTH):
+                print("tuple: ", (object_xval, object_yval), end="\n\n\n\n")
+                map_grid[object_xval][object_yval] = 1
+                print(object_xval, object_yval)
 
-            print(object_xval, object_yval)
         # --- Update angle sweep direction ---
         angle += direction
         if angle >= pan_angle or angle <= -1*pan_angle:
@@ -94,6 +111,7 @@ async def ultrasonic_pan_loop(picarx, current_pos, map_grid):
 
     # Reset pan angle when loop breaks (if ever)
     picarx.set_cam_pan_angle(0)
+    time.sleep(3)
     return map_grid, current_pos   
 
 
@@ -125,4 +143,5 @@ if __name__ == "__main__":
     picar = Picarx(servo_pins=["P0", "P1", "P3"])
     map_ = np.zeros((WIDTH/SAMPLING, LENGTH/SAMPLING))
     current_pos = (X_MID/SAMPLING, 0)
-    asyncio.run(ultrasonic_pan_loop(picar, current_pos, map_))
+    map_ = calibrate(picar, current_pos, map_)
+    print_map(map_)
