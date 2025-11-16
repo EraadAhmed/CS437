f1f86dc0f9bdb5cb17b064bf5c8d8cff25b85888
eahme2 <eahme2@illinois.edu>
2025-11-15 21:08:48 -0600

    changes made to 1.7 client emulator

--- Lab4/lab4_emulator_client.py
+++ Lab4/lab4_emulator_client.py
@@ -9,21 +9,25 @@ import numpy as np
 #TODO 1: modify the following parameters
 #Starting and end index, modify this
 device_st = 1
-device_end = 1661
+device_end = 11
+
+
 
 #Path to the dataset, modify this
 data_path = "data/vehicle{0}.csv"
 
 #Path to your certificates, modify this
-certificate_formatter = "certs/IOTCAR{0}/Car{0}-cert.pem"
-key_formatter = "certs/IOTCAR{0}/Car{0}-priv.pem"
+certificate_formatter = "certs/IOTCar{0}/Car{0}-cert.pem"
+key_formatter = "certs/IOTCar{0}/Car{0}-priv.key"
 
 
 class MQTTClient:
-    def __init__(self, device_id, cert, key):
+    def __init__(self, device_id, cert, key, num):
         # For certificate based connection
         self.device_id = str(device_id)
         self.state = 0
+        self.carnum = num
+        self.thingname = "IOTCar{0}"
         self.client = AWSIoTMQTTClient(self.device_id)
         #TODO 2: modify your broker address
         self.client.configureEndpoint("a1asbxpmh5fugn-ats.iot.us-east-2.amazonaws.com", 8883)
@@ -32,12 +36,15 @@ class MQTTClient:
         self.client.configureDrainingFrequency(2)  # Draining: 2 Hz
         self.client.configureConnectDisconnectTimeout(10)  # 10 sec
         self.client.configureMQTTOperationTimeout(5)  # 5 sec
-        self.client.onMessage = self.customOnMessage
-        
+        # self.client.onMessage = self.customOnMessage
+        self.client.on_message = self.customOnMessage
 
-    def customOnMessage(self,message):
-        #TODO 3: fill in the function to show your received message
-        print("client {} received payload {} from topic {}".format(self.device_id, message ,"vehicle/emission/data" ))
+
+    # def customOnMessage(self,message):
+    #     #TODO 3: fill in the function to show your received message
+    def customOnMessage(self, client, userdata, message):
+        print(f"[{self.device_id}] RECEIVED on {message.topic}: {message.payload.decode()}")
+
 
 
     # Suback callback
@@ -52,24 +59,28 @@ class MQTTClient:
         pass
 
 
-    def publish(self, topic="vehicle/emission/data"):
-    # Load the vehicle's emission data
-        df = pd.read_csv(data_path.format(self.device_id%5))
-        for index, row in df.iterrows():
-            # Create a JSON payload from the row data
-            payload = json.dumps(row.to_dict())
-            
-            # Publish the payload to the specified topic
-            print(f"Publishing: {payload} to {topic}")
-            self.client.publishAsync(topic, payload, 0, ackCallback=self.customPubackCallback)
+    def publish(self, topic="vehicle/emission/data/part1_6"):
+        df = pd.read_csv(data_path.format(self.carnum % 5))
+        row = df.iloc[0].to_dict()           # FIRST ROW ONLY
+        row["thing_name"] = self.thingname.format(self.carnum)
+        payload = json.dumps(row)
+        print(f"Publishing: {payload} to {topic}")
+        self.client.publishAsync(topic, payload, 0, ackCallback=self.customPubackCallback)
+
             
             # Sleep to simulate real-time data publishing
     #subscribe
-    def subscribe(self, topic="vehicle/emission/data", qos=1):
-        def ack_callback(mid):
-            print(f"Subscription for {self.device_id} acknowledged with message id {mid}")
-        self.client.subscribeAsync(topic, qos, self.customOnMessage, ackCallback=ack_callback)
-       
+    def subscribe(self, topic="vehicle/emission/admin/part1_6", qos=1):
+
+        def ack_callback(mid, data):
+            print(f"Subscription for {self.device_id} acknowledged (mid={mid})")
+
+        self.client.subscribeAsync(
+            topic,
+            qos,
+            messageCallback=self.customOnMessage,
+            ackCallback=ack_callback
+        )
 
 
 
@@ -82,10 +93,10 @@ for i in range(5):
 print("Initializing MQTTClients...")
 clients = []
 for device_id in range(device_st, device_end):
-    client = MQTTClient(device_id,certificate_formatter.format(device_id,device_id) ,key_formatter.format(device_id,device_id))
+    client = MQTTClient(device_id,certificate_formatter.format(device_id,device_id) ,key_formatter.format(device_id,device_id), num = device_id)
     client.client.connect()
     clients.append(client)
- 
+
 
 while True:
     print("send now?")
@@ -93,6 +104,10 @@ while True:
     if x == "s":
         for i,c in enumerate(clients):
             c.publish()
+    
+    elif  x == "w":
+        for i,c in enumerate(clients):
+            c.subscribe()
 
     elif x == "d":
         for c in clients:
@@ -103,6 +118,7 @@ while True:
         print("wrong key pressed")
 
     time.sleep(3)
+
 
 
 
